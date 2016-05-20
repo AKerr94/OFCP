a025f11982814cafc890965b2e6b759651a0fe45
Kerr, Alastair <alastair.kerr@sky.uk>
2016-05-20 14:36:49 +0100

    fixed logic

--- build/docker_spawn.sh
+++ build/docker_spawn.sh
@@ -4,9 +4,9 @@
 
 docker images | grep -q ofc-sql-image
 if [ $? -eq 0 ]; then
+    echo "Using existing docker image for ofc-sql container"
+else
     echo "Building new image for ofc-sql container"
     ( cd mysql && docker build --no-cache -t ofc-sql-image . )
-else
-    echo "Using existing docker image for ofc-sql container"
 fi
 docker run -it -d --name ofc-sql-test -p 3306:3306 ofc-sql-image /bin/bash
