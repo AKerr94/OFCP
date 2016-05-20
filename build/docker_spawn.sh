edb92b21ca03c9f2c085fb75108bfe1219d862a6
Kerr, Alastair <alastair.kerr@sky.uk>
2016-05-20 14:33:43 +0100

    Updating docker_spawn.sh - determine whether or not image has been created already

--- build/docker_spawn.sh
+++ build/docker_spawn.sh
@@ -2,6 +2,11 @@
 # Spin up infrastructure with docker
 # Create docker images from Dockerfiles, runs containers
 
-cd mysql 
-docker build --no-cache -t ofc-sql-image .
-docker run -it -d --name test ofc-sql-image /bin/bash
+docker images | grep -q ofc-sql-image
+if [ $? -eq 0 ]; then
+    echo "Building new image for ofc-sql container"
+    ( cd mysql && docker build --no-cache -t ofc-sql-image . )
+else
+    echo "Using existing docker image for ofc-sql container"
+fi
+docker run -it -d --name ofc-sql-test -p 3306:3306 ofc-sql-image /bin/bash
