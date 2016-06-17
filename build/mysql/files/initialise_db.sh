e76ba160a939122970419c092296ba7b69b76390
Kerr, Alastair <alastair.kerr@sky.uk>
2016-06-17 16:24:02 +0100

    First steps to automate setup of restricted access to mysql database by ofcdatabaseuser from docker host

--- build/mysql/files/initialise_db.sh
+++ build/mysql/files/initialise_db.sh
@@ -8,4 +8,11 @@ until mysqladmin ping &>/dev/null; do
 	echo -n "."; sleep 0.2
 done
 
+# Variables used for setting ofcdatabaseuser password and IP to access from
+PASSWORD="password"
+DOCKER_IP=$(ifconfig | grep -A1 docker0 | tail -n 1 | awk '{print $2}')
+
+# Customise sql commands from variables above
+sed "s/password/${PASSWORD}/g" create_ofc_db.sql.template | \
+sed "s/dockerip/${DOCKER_IP}/g" > create_ofc_db.sql
 mysql < /tmp/create_ofc_db.sql
