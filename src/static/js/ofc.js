b4507304f80666426c48bf6cb762fb6eabff3657
Kerr, Alastair <alastair.kerr@sky.uk>
2016-07-15 16:49:20 +0100

    js fix formatting error

--- src/static/js/ofc.js
+++ src/static/js/ofc.js
@@ -8,7 +8,6 @@ function setupGame() {
 	var buttonClicks = 0;
 	button.onclick = function() {
 		buttonClicks++;
-		alert("Game " + game_id + ": Button pressed by player " + player_id + \
-			" " + buttonClicks + " times.")
+		alert("Game " + game_id + ": Button pressed by player " + player_id + " " + buttonClicks + " times.");
 	}
 }
